c5bcbc6d28e64bf00febf725df07de96d16d3c9a
Asmaa-Khaled21 <93935870+Asmaa-Khaled21@users.noreply.github.com>
2022-04-05 14:01:23 +0200

    Final Commit

---
+++ app/src/main/java/com/codingwithme/meowbottomnavigationbar/RecycleView/SettingsAdapter.kt
@@ -0,0 +1,53 @@
+package com.codingwithme.meowbottomnavigationbar.RecycleView
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.codingwithme.meowbottomnavigationbar.R
+
+class SettingsAdapter (var items:List<SettingsItem>): RecyclerView.Adapter<SettingsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_contact,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item=items.get(position)
+        holder.oneItem.setText(item.oneItem)
+        holder.twoItem.setText(item.twoItem)
+        holder.threeItem.setText(item.threeItem)
+        holder.fourItem.setText(item.fourItem)
+        holder.fiveItem.setText(item.fiveItem)
+
+        if (OnButtonClickListener!=null){
+            holder.itemView.setOnClickListener{
+                //  OnItemClickListener.OnItemClick(position,item)
+            }
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+
+    var OnButtonClickListener :OnItemClickListener?=null
+    interface OnItemClickListener{
+        fun OnItemClick(pos:Int ,item:SettingsItem)
+    }
+
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val oneItem : TextView =itemView.findViewById(R.id.House_NUMBER)
+        val twoItem : TextView =itemView.findViewById(R.id.Day)
+        val threeItem : TextView =itemView.findViewById(R.id.GBS)
+        val fourItem : TextView =itemView.findViewById(R.id.clock)
+        val fiveItem : TextView =itemView.findViewById(R.id.money)
+       // val sixItem : Button = itemView.findViewById(R.id.Button_Bay)
+    }
+}
--- app/src/main/java/com/codingwithme/meowbottomnavigationbar/RecycleView/SettingsItem.kt
+++ app/src/main/java/com/codingwithme/meowbottomnavigationbar/RecycleView/SettingsItem.kt
@@ -6,5 +6,6 @@ data class SettingsItem ( val oneItem:String?=null,
                           val twoItem:String?=null,
                           val threeItem:String?=null,
                           val fourItem:String?=null,
-                          val fiveItem:String?=null,
-                          val sixItem:Button?=null)
+                          val fiveItem:String?=null
+                          //val sixItem:Button?=null
+    )
