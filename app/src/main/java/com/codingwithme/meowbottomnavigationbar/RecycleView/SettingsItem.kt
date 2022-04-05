7118d0734b64a801d630823809ce36a431fdead6
Asmaa-Khaled21 <93935870+Asmaa-Khaled21@users.noreply.github.com>
2022-04-05 13:24:49 +0200

    Final Commit

---
+++ app/src/main/java/com/codingwithme/meowbottomnavigationbar/RecycleView/SettingsItem.kt
@@ -0,0 +1,10 @@
+package com.codingwithme.meowbottomnavigationbar.RecycleView
+
+import android.widget.Button
+
+data class SettingsItem ( val oneItem:String?=null,
+                          val twoItem:String?=null,
+                          val threeItem:String?=null,
+                          val fourItem:String?=null,
+                          val fiveItem:String?=null,
+                          val sixItem:Button?=null)
