0152f7413f467e567b383b3dbe448d44ba8fb92f
Asmaa-Khaled21 <93935870+Asmaa-Khaled21@users.noreply.github.com>
2022-04-12 18:28:38 +0200

    Final Commit

--- app/src/main/java/com/codingwithme/meowbottomnavigationbar/PayActivity.kt
+++ app/src/main/java/com/codingwithme/meowbottomnavigationbar/PayActivity.kt
@@ -1,11 +1,44 @@
 package com.codingwithme.meowbottomnavigationbar
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.codingwithme.meowbottomnavigationbar.Fragments.CreditCardFragment
+import com.codingwithme.meowbottomnavigationbar.Fragments.FawryFragment
 
 class PayActivity : AppCompatActivity() {
+
+    lateinit var fawryText:TextView
+    lateinit var creditcardText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pay)
+
+        fawryText = findViewById(R.id.FawrytText)
+        creditcardText = findViewById(R.id.CreditText)
+        startFragment()
+        fawryText.setOnClickListener{
+         supportFragmentManager.beginTransaction()
+             .replace(R.id.frafment_container,CreditCardFragment())
+             .addToBackStack("Fawry")
+             .commit()
+        }
+
+        creditcardText.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frafment_container,FawryFragment())
+                .addToBackStack("CreditCard")
+                .commit()
+        }
     }
+
+    private fun startFragment() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.frafment_container,CreditCardFragment())
+            .addToBackStack("Fawry")
+            .commit()
+    }
+
 }
