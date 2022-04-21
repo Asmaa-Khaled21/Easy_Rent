f539d101a510a9a623a0081139846af72d8c0d44
Asmaa-Khaled21 <93935870+Asmaa-Khaled21@users.noreply.github.com>
2022-04-21 12:22:45 +0200

    Final payment screen

--- app/src/main/java/com/codingwithme/meowbottomnavigationbar/PayActivity.kt
+++ app/src/main/java/com/codingwithme/meowbottomnavigationbar/PayActivity.kt
@@ -1,51 +1,52 @@
 package com.codingwithme.meowbottomnavigationbar
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.codingwithme.meowbottomnavigationbar.Fragments.CreditCardFragment
 import com.codingwithme.meowbottomnavigationbar.Fragments.FawryFragment
 
-class PayActivity : AppCompatActivity() {
 
-    lateinit var fawryText:TextView
-    lateinit var creditcardText: TextView
+class PayActivity : AppCompatActivity() {
+    lateinit var fawryText:Button
+    lateinit var creditcardText:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pay)
 
-
         fawryText = findViewById(R.id.FawrytText)
         creditcardText = findViewById(R.id.CreditText)
-
-        startFragment();
+        startFragment()
 
         fawryText.setOnClickListener{
-          //  fawryText.setBackgroundColor(resources.getColor(R.color.Dark_Blue));
-           // fawryText.setTextColor(resources.getColor(R.color.white))
-           // fawryText .setImageResource(R.drawable.actions_record);
-
-         supportFragmentManager.beginTransaction()
-             .replace(R.id.frafment_container,CreditCardFragment())
+//            creditcardText.setBackgroundColor(resources.getColor(R.color.white))
+//            creditcardText.setTextColor(resources.getColor(R.color.Dark_Blue))
+//            fawryText.setBackgroundColor(resources.getColor(R.color.Dark_Blue))
+//            fawryText.setTextColor(resources.getColor(R.color.white))
+            supportFragmentManager.beginTransaction()
+             .replace(R.id.frafment_container,FawryFragment())
              .addToBackStack("Fawry")
              .commit()
         }
-
         creditcardText.setOnClickListener {
+//            creditcardText.setBackgroundColor(resources.getColor(R.color.Dark_Blue))
+//            creditcardText.setTextColor(resources.getColor(R.color.white))
+//            fawryText.setBackgroundColor(resources.getColor(R.color.white))
+//            fawryText.setTextColor(resources.getColor(R.color.Dark_Blue))
             supportFragmentManager.beginTransaction()
-                .replace(R.id.frafment_container,FawryFragment())
+                .replace(R.id.frafment_container,CreditCardFragment())
                 .addToBackStack("CreditCard")
                 .commit()
         }
     }
 
-    private fun startFragment() {
+   private fun startFragment() {
         supportFragmentManager.beginTransaction()
             .add(R.id.frafment_container,CreditCardFragment())
             .addToBackStack("Fawry")
             .commit()
     }
-
 }
---
+++ app/src/main/java/com/codingwithme/meowbottomnavigationbar/RecycleView/NotificationsItem.kt
@@ -0,0 +1,10 @@
+package com.codingwithme.meowbottomnavigationbar.RecycleView
+
+data class NotificationsItem (
+
+    val Image:Int? =null,
+    val TopText: String? = null,
+    val BottomText: String?=null,
+){
+
+}
